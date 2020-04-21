b404425ce2e45bb08edd8453630a5a1441bc18f0
saitama <klikace123@gmail.com>
2020-04-21 12:45:31 +0700

    first commit up

---
+++ precache-manifest.3e5b638324bbc655d8a562541bf2abe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82cd9e034c94a6517f00f844593fa13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b1910fd7b02a090ceb4",
+    "url": "/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "d63ef8ca0bfb06c315b3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b1910fd7b02a090ceb4",
+    "url": "/static/js/2.4f74d21d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4f74d21d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63ef8ca0bfb06c315b3",
+    "url": "/static/js/main.5011cf37.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
